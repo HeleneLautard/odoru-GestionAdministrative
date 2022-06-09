b13973bd06aec9f7091c561f2abfed2f2b164444
HeleneLautard <helene.lautard@toulouse.miage.fr>
2022-06-09 13:53:25 +0200

    initial commit

---
+++ src/main/java/fr/miage/toulouse/m2/lautard/helene/gestionadministrative/GestionAdministrativeApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m2.lautard.helene.gestionadministrative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAdministrativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAdministrativeApplication.class, args);
+    }
+
+}
